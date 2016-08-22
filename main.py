07f7f6c6516dfb93a320d2574b20deb55e7b0518
Michelle Melton <mailformickie@yahoo.com>
2016-08-22 14:12:09 -0500

    started project

--- main.py
+++ main.py
@@ -16,10 +16,49 @@
 #
 import webapp2
 
+
+
+# caesar.py
+
+ALPHABET_LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def alphabet_position(letter):
+    alphabet = ALPHABET_LOWERCASE if letter.islower() else ALPHABET_UPPERCASE
+    return alphabet.index(letter)
+
+def rotate_char(char, rotation):
+    if not char.isalpha():
+        return char
+
+    alphabet = ALPHABET_LOWERCASE if char.islower() else ALPHABET_UPPERCASE
+    new_pos = (alphabet_position(char) + rotation) % 26
+    return alphabet[new_pos]
+
+def encrypt(text, rotation):
+    answer = ""
+    for char in text:
+        answer += rotate_char(char, rotation)
+    return answer
+
+#create the form to make input
+<form method = "post">
+        <label>Rotate By<input type="text" name = "rotate"></label>
+        <br>
+        <input type = "text" name = "user_text">
+        <br>
+        <input type = "submit">
+</form>
+
+
+
+
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(encrypt(user_text,rotate))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler) debug = True]
 ], debug=True)
